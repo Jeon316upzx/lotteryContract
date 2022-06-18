77b83ea635307907fbb55bda3ac62fd553cb9f43
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-06-18 14:27:59 +0100

    added the lottery contract and a test

---
+++ contracts/Lottery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+
+contract Lottery {
+     
+     address public manager;
+     address[] public players;
+
+     constructor(){
+       manager = msg.sender;
+     }
+
+     function enter() public payable{
+        require(msg.value > 1 ether);
+        players.push(msg.sender);
+     }
+
+     function random() private view returns(uint){
+        uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+     }
+
+     function pickWinner() public onlyManager {
+         uint index = random() % players.length;
+         payable(players[index]).transfer(address(this).balance);
+         players = new address[](0);
+     }
+
+     modifier onlyManager(){
+        require(msg.sender == manager);
+        _;
+
+     }
+
+}
